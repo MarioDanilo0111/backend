92fcf71c1002c8e4e3fbd1c0e830c41d0680f5d3
Mohammd Mustafa <momust187@gmail.com>
2022-03-22 16:22:36 +0100

    Ny kod + css

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ async function renderObjects(){
     // let stringImage = await fetch(apiUrl);
     // let imageObj = await stringImage.json();
     // console.log(apiUrl);
-    let urlLocalhost = "http://localhost:1337/api/Laptops?populate=*";
+    let urlLocalhost = "http://localhost:1337/api/Laptops?populate=";
+    // Url för bild
+    let laptopImgUrl = "/uploads/medium_lenovo_thinkpad_e14_gen2_14_barbar_dator_i58256_gb_svart_pdp_zoom_3000_pdp_main_960_cd1cf28765.webp";
     let stringResponse = await fetch (urlLocalhost);
     let myobject = await stringResponse.json();
     let output = '';
@@ -14,31 +16,34 @@ async function renderObjects(){
     if(Array.isArray(myobject.data)){
         myobject.data.forEach(element => {
             let attr = element.attributes;
-            
-            // for(x in attr){
-            //     console.log(x + ": " + attr[x])
-            // }
-            
-            
+
             output += `
                 <div class="grid-item">
-                    <div>Title: ${attr.Title}</div>
-                    <div>Price: ${attr.Price}</div>
-                    <div >In stock: ${attr.Qty}</div>
+                    <div class="laptop-image">
+                        <img src="${apiUrl + laptopImgUrl}"></img>
+                    </div>
+                    <div class="item-info">
+                        <div class="item-title">${attr.Title}</div>
+                        <div>Price: ${attr.Price}</div>
+                        <div>Qty: ${attr.Qty}</div>
+                    </div>
+                    
                 </div>
+                
             `;
+            
         });
     }else{
+        // Om det bara är ett objekt
         let object = myobject.data.attributes;
-        // for(x in object){
-        //     console.log(x + ": " + object[x])
-        // }
+       
          output += `<div class="title"> Title: ${object.Title}</div>`;
          output += `<div> Description: ${object.Description}</div>`;
          output += `<div> Price: ${object.Price}</div>`;
          output += `<div> In stock: ${object.Qty}</div>`;
     }
      document.getElementById("output").innerHTML = output;
+     document.getElementById("outputPrice").innerHTML = outputPrice;
 }
 
 renderObjects();

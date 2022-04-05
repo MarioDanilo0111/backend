ddc2913fa0d18cbc442738a91adde7a87441376b
IoannisKonstas86 <94480241+IoannisKonstas86@users.noreply.github.com>
2022-04-05 09:36:52 +0200

    Add files via upload
    Html fil Js för att hämta producter och cssn

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 
-
 async function renderObjects(){
     let apiUrl = "http://localhost:1337";
     // let stringImage = await fetch(apiUrl);
     // let imageObj = await stringImage.json();
-    // console.log(apiUrl);
-    let urlLocalhost = "http://localhost:1337/api/Laptops?populate=";
+    
+    let urlLocalhost = "http://localhost:1337/api/keyboards/?populate=Image";
     // Url för bild
-    let laptopImgUrl = "/uploads/medium_lenovo_thinkpad_e14_gen2_14_barbar_dator_i58256_gb_svart_pdp_zoom_3000_pdp_main_960_cd1cf28765.webp";
+  
     let stringResponse = await fetch (urlLocalhost);
     let myobject = await stringResponse.json();
     let output = '';
-    // console.log(myobject);
+    
     //Kolla om data är en array
     if(Array.isArray(myobject.data)){
         myobject.data.forEach(element => {
             let attr = element.attributes;
 
             output += `
-                <div class="grid-item">
-                    <div class="laptop-image">
-                        <img src="${apiUrl + laptopImgUrl}"></img>
-                    </div>
-                    <div class="item-info">
-                        <div class="item-title">${attr.Title}</div>
-                        <div>Price: ${attr.Price}</div>
-                        <div>Qty: ${attr.Qty}</div>
-                    </div>
-                    
+            
+            <div class="col-4" id="${element.id}" onclick="getId(this)">
+            <div class="card h-100 shadow-sm"> <img  src="${apiUrl + attr.Image.data.attributes.formats.thumbnail.url}" class="card-image-top"/>
+              <div class="card-body">
+                <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-primary">Qty:${attr.Qty}</span> <span class="float-end price-hp">${attr.Price}kr</span> </div>
+                    <h5 class="card-title">${attr.Title}</h5>                  
+                    </div>                               
                 </div>
-                
+            </div>
             `;
             
         });
@@ -43,7 +39,46 @@ async function renderObjects(){
          output += `<div> In stock: ${object.Qty}</div>`;
     }
      document.getElementById("output").innerHTML = output;
-     document.getElementById("outputPrice").innerHTML = outputPrice;
+     
 }
 
 renderObjects();
+
+async function getId(item){
+    let product = item.id;
+    console.log(product);
+    let urlLocalhost = "http://localhost:1337/api/keyboards/"+product+"?populate=Image" ;
+    let apiUrl = "http://localhost:1337";
+    let stringResponse = await fetch (urlLocalhost);
+    let myobject = await stringResponse.json();
+    let output = '';
+    let object = myobject.data.attributes;
+       
+
+        output = `
+        <div class="container">
+        <div class="card mb-6" style="max-width: 1980px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${apiUrl + object.Image.data.attributes.formats.medium.url}" class="img-fluid rounded-start">
+          </div>
+          <div class="col-md-6">
+            <div class="card-body">
+              <h5 class="card-title">${object.Title}</h5>
+              <p class="card-text">${object.Description}</p>              
+              <p class="card-text"><span class="float-start badge rounded-pill bg-secondary">Qty:${object.Qty}</span> <span class="float-end price-hp">Price: ${object.Price}kr</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+        
+        
+        
+        `
+
+         document.getElementById("desc").innerHTML = output;
+    }
+     
+
+
